bf8a0bcf2efe048469b595e730d1302d72b2dc55
jhertz <jh.furtado@campus.fct.unl.pt>
2024-04-08 01:16:03 +0100

    Remove unnecessary parenthesis on method declaration

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -23,39 +23,39 @@ typeCheck :: Ast -> Result Type
 typeCheck ast = typeCheck' ast Map.empty
 
 typeCheck' :: Ast -> TypeEnv ->  Result Type
-typeCheck' (Ast { node = Number x }) env = return IntType
-typeCheck' (Ast { node = Bool x })   env = return BoolType
-typeCheck' (Ast { node = Unit })     env = return UnitType
-typeCheck' ast@(Ast { node = Unary MINUS x }) env = checkValue x env IntType
+typeCheck' Ast { node = Number x } env = return IntType
+typeCheck' Ast { node = Bool x }   env = return BoolType
+typeCheck' Ast { node = Unit }     env = return UnitType
+typeCheck' ast@Ast { node = Unary MINUS x } env = checkValue x env IntType
 
 -- arithmetic operators
-typeCheck' ast@(Ast { node = Binary _ PLUS  _ }) env = check ast env IntType IntType
-typeCheck' ast@(Ast { node = Binary _ MINUS _ }) env = check ast env IntType IntType
-typeCheck' ast@(Ast { node = Binary _ TIMES _ }) env = check ast env IntType IntType
-typeCheck' ast@(Ast { node = Binary _ SLASH _ }) env = check ast env IntType IntType
+typeCheck' ast@Ast { node = Binary _ PLUS  _ } env = check ast env IntType IntType
+typeCheck' ast@Ast { node = Binary _ MINUS _ } env = check ast env IntType IntType
+typeCheck' ast@Ast { node = Binary _ TIMES _ } env = check ast env IntType IntType
+typeCheck' ast@Ast { node = Binary _ SLASH _ } env = check ast env IntType IntType
 
 -- logical operators
-typeCheck' ast@(Ast { node = Binary _ AND _ }) env = check ast env BoolType BoolType
-typeCheck' ast@(Ast { node = Binary _ OR  _ }) env = check ast env BoolType BoolType
+typeCheck' ast@Ast { node = Binary _ AND _ } env = check ast env BoolType BoolType
+typeCheck' ast@Ast { node = Binary _ OR  _ } env = check ast env BoolType BoolType
 
 -- comparison operatos 
-typeCheck' ast@(Ast { node = Binary _  GT'  _ }) env = check ast env IntType BoolType
-typeCheck' ast@(Ast { node = Binary _  LT'  _ }) env = check ast env IntType BoolType
-typeCheck' ast@(Ast { node = Binary _ GT_EQ _ }) env = check ast env IntType BoolType
-typeCheck' ast@(Ast { node = Binary _ LT_EQ _ }) env = check ast env IntType BoolType
+typeCheck' ast@Ast { node = Binary _  GT'  _ } env = check ast env IntType BoolType
+typeCheck' ast@Ast { node = Binary _  LT'  _ } env = check ast env IntType BoolType
+typeCheck' ast@Ast { node = Binary _ GT_EQ _ } env = check ast env IntType BoolType
+typeCheck' ast@Ast { node = Binary _ LT_EQ _ } env = check ast env IntType BoolType
 
-typeCheck' ast@(Ast { node = Binary _ EQ_EQ _ }) env = checkEquals ast env
-typeCheck' ast@(Ast { node = Binary _ N_EQ  _ }) env = checkEquals ast env
+typeCheck' ast@Ast { node = Binary _ EQ_EQ _ } env = checkEquals ast env
+typeCheck' ast@Ast { node = Binary _ N_EQ  _ } env = checkEquals ast env
 
 -- TODO: make check more generic so you can use it for this c:
-typeCheck' ast@(Ast { node = Unary NOT _ }) env = checkValue ast env BoolType 
+typeCheck' ast@Ast { node = Unary NOT _ } env = checkValue ast env BoolType 
 
 -- handling identifier c:
-typeCheck' (Ast { node = LetBlock assigns body }) env = do
+typeCheck' Ast { node = LetBlock assigns body } env = do
     newEnv <- foldM mapFunc env assigns
     typeCheck' body newEnv
     where 
-        mapFunc map Assigment{ varName, assignValue, expectedType } = do 
+        mapFunc map Assigment { varName, assignValue, expectedType } = do 
             typ <- typeCheck' assignValue map
             maybe (insert typ) (\(typ', token) -> 
                 if typ' == typ then 
@@ -66,13 +66,13 @@ typeCheck' (Ast { node = LetBlock assigns body }) env = do
             where 
                 insert typ = return $ Map.insert varName typ map
 
-typeCheck' (Ast { token, node = Var name }) env = 
+typeCheck' Ast { token, node = Var name } env = 
     maybe (makeError token $ "Undefined identifier: " ++ name) 
             return $  Map.lookup name env 
 
 -- Helper function c:
 check :: Ast -> TypeEnv -> Type -> Type -> Result Type
-check (Ast { token, node = Binary left _ right }) env expected result = do
+check Ast { token, node = Binary left _ right } env expected result = do
     left'  <- typeCheck' left  env
     right' <- typeCheck' right env
 
@@ -83,7 +83,7 @@ check (Ast { token, node = Binary left _ right }) env expected result = do
                     ++ show left' ++ "' type and '" ++ show right' ++ "' type."
 
 checkValue :: Ast -> TypeEnv -> Type -> Result Type -- TODO: finish this
-checkValue (Ast { token, node = Unary _ child }) env expected = do
+checkValue Ast { token, node = Unary _ child } env expected = do
     exprT <- typeCheck' child env
     if exprT == expected 
         then return expected
@@ -91,7 +91,7 @@ checkValue (Ast { token, node = Unary _ child }) env expected = do
             ++ show expected ++ "' type but found a/an '" ++ show exprT ++ "' type."
 
 checkEquals :: Ast -> TypeEnv -> Result Type
-checkEquals (Ast { token, node = Binary left _ right } ) env =  do
+checkEquals Ast { token, node = Binary left _ right }  env =  do
     left'  <- typeCheck' left env
     right' <- typeCheck' right env
 
@@ -102,7 +102,7 @@ checkEquals (Ast { token, node = Binary left _ right } ) env =  do
                               ++ show left' ++ "' type and '" ++ show right' ++ "' type."
 
 makeError :: Token -> String -> Result a
-makeError (Token { Scanner.line, Scanner.position }) msg = 
+makeError Token { Scanner.line, Scanner.position } msg = 
     Left $ Error { 
                    errType  = TypingError, 
                    message  = msg, 
